3f6b1ba72efd9aa6dc4709e5645e083c81ee5372
Lucas Caballero <caballerolucas18@gmail.com>
2026-04-15 17:01:52 -0300

    prueba menu slid

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function toggleMenu() {
   btn.classList.toggle('open');
   body.classList.toggle('opacity')
 }
+
+
+document.addEventListener('click', function(event) {
+  const menu = document.getElementById('side-menu');
+  const btn = document.getElementById('menuBtn');
+  const body = document.getElementById('body')
+ 
+  const isMenuOpen = menu.classList.contains('active');
+  
+
+  const clickedOutside = !menu.contains(event.target) && !btn.contains(event.target);
+
+  if (isMenuOpen && clickedOutside) {
+
+    menu.classList.remove('active');
+    btn.classList.remove('open');
+    body.classList.toggle('opacity')
+  }
+});
+
